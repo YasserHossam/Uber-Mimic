423124e472bd76b7bb3e354fba3814af10c302e8
Yasser Hossam <yasserhossam.1994@gmail.com>
2022-01-13 16:06:45 +0200

    oinitial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+"#MTM's tasks" 
+
+A simple uber-like application representing ordering a driver 
+
+## Dependencies / Third part liberaries :
+
+
+## language :
+
+Kotlin
+
+
+##
+
+## Note
+
---
+++ app/src/test/java/com/mtm/uber_mimic/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.mtm.uber_mimic
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
